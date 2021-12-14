7a8f51e67e765b1fde8d149c58fbd6d6438c377b
Prajwal Horti <hortiprajwal@gmail.com>
2021-12-14 17:26:26 +0530

    Firebase auth

---
+++ lib/services/auth.dart
@@ -0,0 +1,27 @@
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // sign in anon
+  Future signInAnon() async {
+    try {
+        AuthResult result = await _auth.signInAnonymously();
+        FirebaseUser user = result.user;
+        return user;
+    }
+    catch(e) {
+        print(e.toString());
+        return null;
+    }
+}
+
+  //sign in wit email & password
+
+  //register with email & password
+
+  //sign out
+
+}
