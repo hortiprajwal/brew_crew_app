98b49d4981eb5f6690933f9197002b64b7ed36e8
Prajwal Horti <hortiprajwal@gmail.com>
2021-12-16 17:36:05 +0530

    Customer User Model-Instance not working

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,16 +1,22 @@
 // ignore: import_of_legacy_library_into_null_safe
+import 'package:brew_crew/models/myuser.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  //create MyUser obj based on FirebaseUser
+  MyUser? _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? MyUser(uid: user.uid) : null;
+  }
+
   // sign in anon
   Future signInAnon() async {
     try {
         AuthResult result = await _auth.signInAnonymously();
         FirebaseUser user = result.user;
-        return user;
+        return _userFromFirebaseUser(user);
     }
     catch(e) {
         print(e.toString());
